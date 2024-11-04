e9ffa657eaf8df9aa07a28cd18ddb29c34e3e371
unknown <6721061001@cdti.ac.th>
2024-11-04 12:50:33 +0700

    kaodum

---
+++ BMI.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cmath>
+
+float calculate(float weight, float height) {
+	float result = weight/pow(height, 2);
+	return result;
+}
+
+main() {
+	float weight, height, bmi;
+	printf("Enter your weight. ");
+	scanf("%f", &weight);
+	printf("Enter your height. ");
+	scanf("%f", &height);
+	height = height/100;
+	bmi = calculate(weight, height);
+	printf("Your BMI is %.2f. \n", bmi);
+	
+	if (bmi < 18.5){
+		printf("You are underweight");
+	}else if (bmi >= 18.5 && bmi <24.9)	{
+		printf("You are normal");	
+	}else if (bmi >=25 && bmi <29.9){
+		printf("You are overweight");
+	}else if (bmi >30) {
+		printf("Please consult a doctor!!");
+	}
+}
